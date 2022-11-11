eae9583cf4dd63dd5593ccdcbcb835ddad923b99
headersalreadysent <headersalreadysent@users.noreply.github.com>
2022-11-11 14:52:32 +0000

    send agent to scraper

--- src/Youtube-Scraper.js
+++ src/Youtube-Scraper.js
@@ -3,7 +3,7 @@ const htmlParser = require('./HtmlParser')
 class YoutubeScraper {
   // starting point
   static async scrapeTrendingPage({ page = 'default', geoLocation = null, parseCreatorOnRise = false,agent=null } = {}) {
-    const requestData = await requester.requestTrendingPage(geoLocation, page)
+    const requestData = await requester.requestTrendingPage(geoLocation, page, agent)
     return htmlParser.parseNewHtml(requestData.data, parseCreatorOnRise)
   }
 }
